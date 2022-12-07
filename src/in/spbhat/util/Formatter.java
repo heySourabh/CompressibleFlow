d80deb5e10fba15235a7837852d9f79b2cce32a5
Sourabh Bhat <heysourabh@gmail.com>
2022-12-07 22:34:23 +0100

    Updated Formatter class to remove bug with exponents

--- src/in/spbhat/util/Formatter.java
+++ src/in/spbhat/util/Formatter.java
@@ -12,6 +12,11 @@ public class Formatter {
     }
 
     private static String removeTrailingZeros(String doubleValueStr) {
+        if (doubleValueStr.contains("e") || doubleValueStr.contains("E")) {
+            String[] parts = doubleValueStr.split("[eE]");
+            return removeTrailingZeros(parts[0]) + "e" + parts[1];
+        }
+
         if (doubleValueStr.contains(".") && doubleValueStr.endsWith("0")) {
             return removeTrailingZeros(doubleValueStr.substring(0, doubleValueStr.length() - 1));
         } else if (doubleValueStr.endsWith(".")) {
@@ -19,5 +24,13 @@ public class Formatter {
         } else {
             return doubleValueStr;
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(doubleToString(4545.3545));
+        System.out.println(doubleToString(1.25245345e10));
+        System.out.println(doubleToString(0.00000000457));
+        System.out.println(doubleToString(123.567));
+        System.out.println(doubleToString(1235000.34));
     }
 }
