425a16dc12ec7715211001b47d4982da8b6af349
Sourabh Bhat <heySourabh@gmail.com>
2022-12-10 14:54:42 +0100

    Added km_h to Speed class

--- src/in/spbhat/physics/Speed.java
+++ src/in/spbhat/physics/Speed.java
@@ -2,12 +2,12 @@ package in.spbhat.physics;
 
 import in.spbhat.util.Formatter;
 
-import static in.spbhat.physics.Speed.Units.ft_s;
-import static in.spbhat.physics.Speed.Units.m_s;
+import static in.spbhat.physics.Speed.Units.*;
 
 public class Speed {
     enum Units {
         m_s(1.0, "m/s"),
+        km_h(1.0 / 3.6, "km/h"),
         ft_s(0.3048, "ft/s");
 
         private final double conversion;
@@ -50,5 +50,8 @@ public class Speed {
         System.out.println(new Speed(123, m_s));
         System.out.println(new Speed(123, m_s).to(ft_s));
         System.out.println(new Speed(4.5, ft_s).to(m_s));
+        System.out.println(new Speed(234, km_h).to(m_s));
+        System.out.println(new Speed(65.47, m_s).to(km_h));
+        System.out.println(new Speed(862.6, km_h).to(ft_s));
     }
 }
