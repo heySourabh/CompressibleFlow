38b369298207883541f991ed72674b93ff230d43
Sourabh Bhat <heySourabh@gmail.com>
2022-12-11 23:01:44 +0100

    Added MachRegime

--- src/in/spbhat/physics/Mach.java
+++ src/in/spbhat/physics/Mach.java
@@ -8,12 +8,25 @@ import static in.spbhat.geometry.Angle.asin;
 import static in.spbhat.geometry.Angle.sin;
 
 public record Mach(double machNumber) {
+    public enum MachRegime {
+        Subsonic, Supersonic, Sonic
+    }
+
     public Angle machAngle() {
         return asin(1.0 / machNumber);
     }
 
     public static Mach calculateMach(Angle machAngle) {
         return new Mach(1.0 / sin(machAngle));
+    }
+
+    public MachRegime machRegime() {
+        if (this.machNumber < 1)
+            return MachRegime.Subsonic;
+        else if (this.machNumber > 1)
+            return MachRegime.Supersonic;
+        else
+            return MachRegime.Sonic;
     }
 
     @Override
@@ -25,5 +38,11 @@ public record Mach(double machNumber) {
         System.out.println(new Mach(5));
         System.out.println(new Mach(2).machAngle());
         System.out.println(Mach.calculateMach(new Angle(30, degrees)));
+
+        System.out.println(new Mach(5).machRegime());
+        System.out.println(new Mach(1.00001).machRegime());
+        System.out.println(new Mach(1.0).machRegime());
+        System.out.println(new Mach(0.999).machRegime());
+        System.out.println(new Mach(0.1).machRegime());
     }
 }
