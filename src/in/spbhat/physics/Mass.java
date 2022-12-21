a6b26513a912596f120f8e9dc92385fc1db8a2b5
Sourabh Bhat <heySourabh@gmail.com>
2022-12-21 23:24:12 +0100

    Tests for Mass class

--- src/in/spbhat/physics/Mass.java
+++ src/in/spbhat/physics/Mass.java
@@ -10,10 +10,8 @@ import in.spbhat.geometry.Volume;
 import in.spbhat.util.Formatter;
 
 import static in.spbhat.gas.properties.Density.Units.kg_m3;
-import static in.spbhat.gas.properties.Density.Units.pound_ft3;
-import static in.spbhat.geometry.Volume.Units.*;
+import static in.spbhat.geometry.Volume.Units.cubic_m;
 import static in.spbhat.physics.Mass.Units.kg;
-import static in.spbhat.physics.Mass.Units.pound;
 
 public class Mass {
     public enum Units {
@@ -42,6 +40,10 @@ public class Mass {
         this.units = units;
     }
 
+    public Mass(Density density, Volume volume) {
+        this(density.in(kg_m3) * volume.in(cubic_m), kg);
+    }
+
     public double in(Units units) {
         double mass_kg = this.value * this.units.conversion;
         return mass_kg / units.conversion;
@@ -51,22 +53,8 @@ public class Mass {
         return new Mass(in(units), units);
     }
 
-    public static Mass calculateMass(Density density, Volume volume) {
-        return new Mass(density.in(kg_m3) * volume.in(cubic_m), kg);
-    }
-
     @Override
     public String toString() {
-        return Formatter.doubleToString(this.value) + " " + this.units.unitStr;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Mass(123, kg));
-        System.out.println(new Mass(1.4, pound));
-        System.out.println(new Mass(5.5, kg).to(pound));
-        System.out.println(new Mass(82.5, pound).to(kg));
-
-        System.out.println(Mass.calculateMass(new Density(1.2, kg_m3), new Volume(2, cubic_cm)));
-        System.out.println(Mass.calculateMass(new Density(12, pound_ft3), new Volume(2, cubic_ft)));
+        return Formatter.doubleToString(this.value) + " " + this.units;
     }
 }
---
+++ test/in/spbhat/physics/MassTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022.
+ * author: Sourabh Bhat <heySourabh@gmail.com>
+ */
+
+package in.spbhat.physics;
+
+import in.spbhat.gas.properties.Density;
+import in.spbhat.geometry.Volume;
+import org.junit.jupiter.api.Test;
+
+import static in.spbhat.gas.properties.Density.Units.kg_m3;
+import static in.spbhat.geometry.Volume.Units.cubic_m;
+import static in.spbhat.physics.Mass.Units.kg;
+import static in.spbhat.physics.Mass.Units.pound;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MassTest {
+
+    @Test
+    void in() {
+        // kg -> kg
+        assertEquals(437.920, new Mass(437.920, kg).in(kg));
+        // kg -> pound
+        assertEquals(60.030330757, new Mass(27.2293, kg).in(pound), 1e-8);
+        // pound -> pound
+        assertEquals(158.859, new Mass(158.859, pound).in(pound));
+        // pound -> kg
+        assertEquals(200.03332799, new Mass(440.998, pound).in(kg), 1e-8);
+    }
+
+    @Test
+    void to() {
+        // kg -> kg
+        assertEquals("437.92 kg", new Mass(437.920, kg).to(kg).toString());
+        // kg -> pound
+        assertEquals("60.0303 lbm", new Mass(27.2293, kg).to(pound).toString());
+        // pound -> pound
+        assertEquals("158.859 lbm", new Mass(158.859, pound).to(pound).toString());
+        // pound -> kg
+        assertEquals("200.033 kg", new Mass(440.998, pound).to(kg).toString());
+    }
+
+    @Test
+    void toStringTest() {
+        assertEquals("332.897 kg", new Mass(332.897, kg).toString());
+        assertEquals("436.388 lbm", new Mass(436.388, pound).toString());
+        assertEquals("3 kg", new Mass(
+                new Density(1.5, kg_m3),
+                new Volume(2, cubic_m)).toString());
+    }
+}
