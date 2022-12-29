6e77c3ea24dc323a5bcc022df4a11ec5958a6c23
Sourabh Bhat <heySourabh@gmail.com>
2022-12-30 00:43:12 +0100

    Added ShockAngle and DeflectionAngle classes

---
+++ src/in/spbhat/geometry/DeflectionAngle.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2022.
+ * author: Sourabh Bhat <heySourabh@gmail.com>
+ */
+
+package in.spbhat.geometry;
+
+public class DeflectionAngle extends Angle {
+    public DeflectionAngle(double value, Units units) {
+        super(value, units);
+    }
+}
---
+++ src/in/spbhat/geometry/ShockAngle.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2022.
+ * author: Sourabh Bhat <heySourabh@gmail.com>
+ */
+
+package in.spbhat.geometry;
+
+public class ShockAngle extends Angle {
+    public ShockAngle(double value, Units units) {
+        super(value, units);
+    }
+}
